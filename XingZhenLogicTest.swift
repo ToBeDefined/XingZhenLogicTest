2ed8fa22f97f54112408838f91d46661de3ebda0
TBD <weinanshao@163.com>
2018-03-02 19:50:52 +0800

    修改使用循环递归，不使用四叉树，优化存储和创建对象，速度提升

--- XingZhenLogicTest.swift
+++ XingZhenLogicTest.swift
@@ -11,148 +11,56 @@ import Foundation
 
 // MARK: DataModel
 
-class SelectTreeNode {
-    enum Select: Int {
-        case None = -1
-        case A = 0
-        case B = 1
-        case C = 2
-        case D = 3
-        var description: String {
-            switch self {
-            case .A:
-                return "A"
-            case .B:
-                return "B"
-            case .C:
-                return "C"
-            case .D:
-                return "D"
-            case .None:
-                return " ERROR "
-            }
-        }
-        
-    }
-    
-    var data: Select?
-    var a: SelectTreeNode?
-    var b: SelectTreeNode?
-    var c: SelectTreeNode?
-    var d: SelectTreeNode?
-    
-    var parentNode: SelectTreeNode?
-    
+enum Select: Int {
+    case None = -1
+    case A = 0
+    case B = 1
+    case C = 2
+    case D = 3
     var description: String {
-        guard let data = self.data, data != .None else {
-            return "NULL"
+        switch self {
+        case .A:
+            return "A"
+        case .B:
+            return "B"
+        case .C:
+            return "C"
+        case .D:
+            return "D"
+        case .None:
+            return " ERROR "
         }
-        return data.description
     }
 }
 
-func initSelectTree(rootNode: SelectTreeNode?, deep: Int) {
-    if deep == 0 {
-        return
-    }
-    
-    rootNode?.a = SelectTreeNode()
-    rootNode?.a?.parentNode = rootNode
-    rootNode?.a?.data = .A
-    
-    rootNode?.b = SelectTreeNode()
-    rootNode?.b?.parentNode = rootNode
-    rootNode?.b?.data = .B
-    
-    rootNode?.c = SelectTreeNode()
-    rootNode?.c?.parentNode = rootNode
-    rootNode?.c?.data = .C
-    
-    rootNode?.d = SelectTreeNode()
-    rootNode?.d?.parentNode = rootNode
-    rootNode?.d?.data = .D
-    
-    initSelectTree(rootNode: rootNode?.a, deep: deep - 1)
-    initSelectTree(rootNode: rootNode?.b, deep: deep - 1)
-    initSelectTree(rootNode: rootNode?.c, deep: deep - 1)
-    initSelectTree(rootNode: rootNode?.d, deep: deep - 1)
-}
-
-func getAllLeafNode(of node: SelectTreeNode?) -> [SelectTreeNode] {
-    var leafNodeArray: [SelectTreeNode] = []
-    func getLeafNode(of node: SelectTreeNode?) {
-        guard let node = node else { return }
-        if node.a == nil && node.b == nil && node.c == nil && node.d == nil {
-            leafNodeArray.append(node)
-        }
-        
-        if node.a != nil {
-            getLeafNode(of: node.a)
-        }
-        
-        if node.b != nil {
-            getLeafNode(of: node.b)
-        }
-        
-        if node.c != nil {
-            getLeafNode(of: node.c)
-        }
-        
-        if node.d != nil {
-            getLeafNode(of: node.d)
-        }
-    }
-    getLeafNode(of: node)
-    return leafNodeArray
-}
-
-func getPath(of leafNode: SelectTreeNode?) -> [SelectTreeNode.Select] {
-    if leafNode == nil {
-        return []
-    }
-    
-    var path = [SelectTreeNode.Select]()
-    var node: SelectTreeNode? = leafNode
-    while node?.parentNode != nil {
-        path.insert((node?.data)!, at: 0)
-        node = node?.parentNode
-    }
-    return path
-}
-
-func printPath(_ path: [SelectTreeNode.Select]) {
+func printAnswers(_ answers: [Select]) {
     var str = ""
-    for item in path {
+    for item in answers {
         str += item.description
     }
     print(str)
 }
 
-func traverseSelectTree(rootNode: SelectTreeNode?) {
-    let leafNodeArray = getAllLeafNode(of: rootNode)
-    for node in leafNodeArray {
-        printPath(getPath(of: node))
-    }
-}
+
 
 // MARK: Logic Test
 
-func isTruePath(_ path: [SelectTreeNode.Select]) -> Bool {
-    var path = path
+func isTrueAnswers(_ answers: [Select]) -> Bool {
+    var answers = answers
     // 方便从1开始
-    path.insert(SelectTreeNode.Select.None, at: 0)
+    answers.insert(Select.None, at: 0)
     // 题1 不用管
     // 题2
-    if (path[2] == .A && path[5] != .C) ||
-        (path[2] == .B && path[5] != .D) ||
-        (path[2] == .C && path[5] != .A) ||
-        (path[2] == .D && path[5] != .B) {
+    if (answers[2] == .A && answers[5] != .C) ||
+        (answers[2] == .B && answers[5] != .D) ||
+        (answers[2] == .C && answers[5] != .A) ||
+        (answers[2] == .D && answers[5] != .B) {
         return false
     }
     
     // 题3
-    var threeAnswerArray = [path[3], path[6], path[2], path[4]]
-    let threeAnswerIndex = path[3].rawValue
+    var threeAnswerArray = [answers[3], answers[6], answers[2], answers[4]]
+    let threeAnswerIndex = answers[3].rawValue
     let diffrentAnswer = threeAnswerArray.remove(at: threeAnswerIndex)
     let equealAnswerSet = Set.init(threeAnswerArray)
     // 剩下的不是同一个答案
@@ -165,27 +73,27 @@ func isTruePath(_ path: [SelectTreeNode.Select]) -> Bool {
     }
     
     // 题4
-    let fourAnswerIndex = path[4].rawValue
+    let fourAnswerIndex = answers[4].rawValue
     var fourAnswerArray: [[Int]] = [[1, 5], [2, 7], [1, 9], [6, 10]]
     var foueAnswer = fourAnswerArray[fourAnswerIndex]
-    if path[foueAnswer[0]] != path[foueAnswer[1]]{
+    if answers[foueAnswer[0]] != answers[foueAnswer[1]]{
         return false
     }
     
     // 题5
     var fiveAnswerArray: [Int] = [8, 4, 9, 7]
-    let fiveAnswerIndex = path[5].rawValue
+    let fiveAnswerIndex = answers[5].rawValue
     let fiveAnswer = fiveAnswerArray[fiveAnswerIndex]
-    if path[5] != path[fiveAnswer] {
+    if answers[5] != answers[fiveAnswer] {
         return false
     }
     
     // 题6
     var sixAnswerArray:[[Int]] = [[2, 4], [1, 6], [3, 10], [5, 9]]
     // 答案给的题号
-    var sixAnswer = sixAnswerArray[path[6].rawValue]
+    var sixAnswer = sixAnswerArray[answers[6].rawValue]
     // 对应题号的答案
-    let sixAnswersAnswerArray = [path[sixAnswer[0]], path[sixAnswer[1]], path[8]]
+    let sixAnswersAnswerArray = [answers[sixAnswer[0]], answers[sixAnswer[1]], answers[8]]
     // 对应题号的答案Set
     let sixAnswersAnswerSet = Set.init(sixAnswersAnswerArray)
     if sixAnswersAnswerSet.count != 1 {
@@ -197,7 +105,7 @@ func isTruePath(_ path: [SelectTreeNode.Select]) -> Bool {
     var BCount = 0
     var CCount = 0
     var DCount = 0
-    for select in path {
+    for select in answers {
         switch select {
         case .A:
             ACount += 1
@@ -212,9 +120,9 @@ func isTruePath(_ path: [SelectTreeNode.Select]) -> Bool {
         }
     }
     var abcdCountArray = [ACount, BCount, CCount, DCount]
-    var sevenAnswerArray = [SelectTreeNode.Select.C, SelectTreeNode.Select.B,  SelectTreeNode.Select.A, SelectTreeNode.Select.D]
+    var sevenAnswerArray = [Select.C, Select.B, Select.A, Select.D]
     // 获取给的答案是ABCD哪个
-    let sevenAnswer = sevenAnswerArray[path[7].rawValue]
+    let sevenAnswer = sevenAnswerArray[answers[7].rawValue]
     // 从abcdCountArray取出对应字母的出现次数
     let minCountAnswer = abcdCountArray[sevenAnswer.rawValue]
     if minCountAnswer != min(ACount, BCount, CCount, DCount) {
@@ -224,9 +132,9 @@ func isTruePath(_ path: [SelectTreeNode.Select]) -> Bool {
     
     // 题8
     var eightAnswerArray = [7, 5, 2, 10]
-    let eightAnswer = eightAnswerArray[path[8].rawValue]
-    let eightAnswersAnswer = path[eightAnswer]
-    let firstAnswer = path[1]
+    let eightAnswer = eightAnswerArray[answers[8].rawValue]
+    let eightAnswersAnswer = answers[eightAnswer]
+    let firstAnswer = answers[1]
     let value1 = firstAnswer.rawValue
     let value2 = eightAnswersAnswer.rawValue
     // 相邻 = 1
@@ -235,11 +143,11 @@ func isTruePath(_ path: [SelectTreeNode.Select]) -> Bool {
     }
     
     // 题9
-    let boolen16 = (path[1] == path[6])
+    let boolen16 = (answers[1] == answers[6])
     var nineAnswerArray = [6, 10, 2, 9]
-    let nineAnswer = nineAnswerArray[path[9].rawValue]
-    let nineAnswersAnswer = path[nineAnswer]
-    let boolenx5 = (path[5] == nineAnswersAnswer)
+    let nineAnswer = nineAnswerArray[answers[9].rawValue]
+    let nineAnswersAnswer = answers[nineAnswer]
+    let boolenx5 = (answers[5] == nineAnswersAnswer)
     // 1/6 与 x/5 相反
     if boolen16 == boolenx5 {
         return false
@@ -250,24 +158,37 @@ func isTruePath(_ path: [SelectTreeNode.Select]) -> Bool {
     let minABCDCount = min(ACount, BCount, CCount, DCount)
     let distance = maxABCDCount - minABCDCount
     var tenAnswerArray = [3, 2, 4, 1]
-    if distance != tenAnswerArray[path[10].rawValue] {
+    if distance != tenAnswerArray[answers[10].rawValue] {
         return false
     }
     return true
 }
 
+
 // MARK: Start
 
-var rootNode: SelectTreeNode? = SelectTreeNode()
-initSelectTree(rootNode: rootNode, deep: 10)
-let array = getAllLeafNode(of: rootNode)
-var truePathCount = 0
-for leafNode in array {
-    let path = getPath(of: leafNode)
-    if isTruePath(path) {
-        truePathCount += 1
-        printPath(path)
+let selectArray: [Select] = [.A, .B, .C, .D]
+
+var answers: [Select] = []
+var trueAnswersCount = 0
+func getAnswer(for index: Int = 0) {
+    if index >= 10 {// 0...9
+        // 判断答案
+        if isTrueAnswers(answers) {
+            trueAnswersCount += 1
+            printAnswers(answers)
+        }
+        return
+    }
+    for answer in selectArray {
+        answers.append(answer)
+        getAnswer(for: (index + 1))
+        answers.removeLast()
     }
 }
-print("总共有 \(truePathCount) 种答案")
+
+getAnswer()
+print("总共有 \(trueAnswersCount) 种答案")
+
+
 
